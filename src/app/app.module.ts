0dbd0cdd51b1ae51a05c8862cec378fd3a8f5472
Patricio2308 <patricioandres00@gmail.com>
2023-01-15 10:52:09 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { TaskComponent } from './components/task/task.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    TaskFormComponent,
+    TaskComponent,
+    TaskListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
