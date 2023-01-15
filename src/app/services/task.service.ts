e7c2dff8dea7b81c407ee50304d597d8d1ab787c
Patricio2308 <patricioandres00@gmail.com>
2023-01-15 12:27:34 -0300

    servicio de tareas

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,11 +1,34 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../models/task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  tasks: Task[] = [];    
 
   constructor() {
-    
+    this.tasks = [
+      {
+        title: "Crear Front",
+        descripcion: "Diseño y estilos"
+      },
+      {
+        title: "Crear Back",
+        descripcion: "Diseñar  y programar API"
+      },
+      {
+        title: "Crear Base de Datos",
+        descripcion: "Diseñar UML y tablas"
+      }
+    ];
+   }
+
+   getTasks(){
+    return this.tasks;
+   }
+
+   addTask(task: Task){
+    this.tasks.push(task);
    }
 }
